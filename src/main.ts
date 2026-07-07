84fc1a5529adb2fe7307550debf9b63711ed8585
Muhammed Rashid N <muhammed916rashid@gmail.com>
2026-07-07 13:33:25 +0530

    chore: update default application port from 3000 to 4000

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
 }
 bootstrap();
